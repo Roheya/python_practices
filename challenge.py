3a76898c3a59b3e110194eaa45104923a334d311
Roheya20 <tourayroheya7@gmail.com>
2026-09-25 18:22:30 +0200

    using if statements

---
+++ challenge.py
@@ -0,0 +1,17 @@
+num = int(input("Enter a number:"))
+if num > 0:
+    print("The number you entered is positive")
+else:
+    print("The number you entered is negative")
+
+if num % 2 == 0:
+    print("The number you entered is even")
+else:
+    print("The number you entered is odd")
+
+
+if num > 100 :
+    print("The number you entered is greater than 100")
+else:
+    print("The number you entered is less than or equal to 100")
+    
