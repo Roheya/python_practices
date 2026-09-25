802e57ac17ca2b560cd6ef300a743039e88ff199
Roheya20 <tourayroheya7@gmail.com>
2026-09-25 18:17:51 +0200

    user input and f string

---
+++ question2.py
@@ -0,0 +1,5 @@
+name = input("Enter your name:")
+age = int(input("Enter your age:"))
+color =input("What's your favorite color:")
+
+print(f"My name is {name}, I am {age} years old, and my favorite color is {color}")
